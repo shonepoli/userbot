c62ec49887bafab967ce2662b9b1dedd75ea4f7d
shonepoli <63222799+shonepoli@users.noreply.github.com>
2020-04-06 10:53:17 +0530

    Update Bloom.py

--- stdplugins/Bloom.py
+++ stdplugins/Bloom.py
@@ -30,7 +30,7 @@ FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
 
 @borg.on(admin_cmd(pattern="bloom ?(.*)"))
 async def autopic(event): 
-    await event.edit("Bloom colour profile pic have been enabled © @AnonymousD3061") 
+    await event.edit("Bloom colour profile pic have been enabled © @CHASTISER_OFFICIAL") 
     downloaded_file_name = "./ravana/original_pic.png"
     downloader = SmartDL(Config.RAVANA_LEELA, downloaded_file_name, progress_bar=True)
     downloader.start(blocking=False)
